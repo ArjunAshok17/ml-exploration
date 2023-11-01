25bac61d7e080a439b1e8780407175575323b211
Arjun Ashok <arjun3.ashok@gmail.com>
2023-10-31 18:46:20 -0700

    setup autoregressive for later

---
+++ regression-models/auto_regressive.py
@@ -0,0 +1,16 @@
+"""
+    @brief exploration into different auto-regressive models and how they fair 
+    with respect to each other against cyclical, non-cyclical, and trend-stable 
+    noisy data.
+    @author Arjun Ashok (arjun3.ashok@gmail.com)
+    @creation-data October 2023
+"""
+
+
+# ----------------------------- Environment Setup ---------------------------- #
+
+
+# ------------------------------ Testing Models ------------------------------ #
+if __name__ == "__main__":
+    # data load & preprocess
+    
